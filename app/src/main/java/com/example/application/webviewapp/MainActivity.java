347f6dbf758d6958b354a78015c5b56c23eb9dcb
ember2000 <zshan@email.wm.edu>
2022-03-07 09:38:55 -0500

    Added zoom and pinch

--- app/src/main/java/com/example/application/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/application/webviewapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.application.webviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,8 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // webview
         webView = (WebView)findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com");
+        webView.loadUrl("https://mdpmap.azurewebsites.net/index.html");
+
+        // enable zoom and pinch controls
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false); //hide the zoom buttons
+
     }
+
+
 }
